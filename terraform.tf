123cdc9e92a095a62b79bce05f9f8d3c78c551e8
lmarqs <lucas.marques@medprevonline.com>
2024-09-08 15:17:36 -0300

    [main] save state to cloud

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.8"
 
+  backend "remote" {
+    organization = "lmarqs"
+
+    workspaces {
+      name = "server-local"
+    }
+  }
+
   required_providers {
     lxd = {
       source  = "terraform-lxd/lxd"
